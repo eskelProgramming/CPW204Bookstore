a3773a49e619a561cc34a113edb630b82eb3e3d6
eskelProgramming <eskelley0673@students.cptc.edu>
2024-04-23 18:11:22 -0700

    Documented Book class

--- ts/main.ts
+++ ts/main.ts
@@ -2,9 +2,25 @@
  * Represents a individual book that can be purchased
  */
 class Book {
+    /**
+     * The 13 digit ISBN number
+     */
     isbn : string;
+
+    /**
+     * The title of the book
+     */
     title : string;
+
+    /**
+     * The retail price of the individual book
+     */
     price : number;
+
+    /**
+     * The date the book was first published. This could 
+     * be a future date, if the book is not yet released.
+     */
     releaseDate : Date;
 }
 
